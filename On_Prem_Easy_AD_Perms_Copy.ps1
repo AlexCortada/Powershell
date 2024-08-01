03ebc08ac667c208d04c4c1e3a7a7cdb57e00a68
Alex Cortada <alexander.cortada@gmail.com>
2024-08-01 19:56:23 -0400

    Update On_Prem_Easy_AD_Perms_Copy.ps1

--- On_Prem_Easy_AD_Perms_Copy.ps1
+++ On_Prem_Easy_AD_Perms_Copy.ps1
@@ -1,3 +1,11 @@
+# Author: Alex Cortada
+# Description: A super simple Powershell script that grabs permissions from one user and adds it to another.
+# Date: 08.01.2024
+# Version: 2024.08.01
+
+# Script begins here
+
+#Add the usernames in the FROMACCOUNT and TOACCOUNT sections. Verify all changes in AD when finished.
 $CopyFromUser = Get-ADUser FROMACCOUNT -prop MemberOf
 $CopyToUser = Get-ADUser TOACCOUNT -prop MemberOf
 $CopyFromUser.MemberOf | Where{$CopyToUser.MemberOf -notcontains $_} |  Add-ADGroupMember -Members $CopyToUser

d9d4df681bd2a1b6d9606820076aa463e1741045
Alex Cortada <alexander.cortada@gmail.com>
2024-08-01 19:54:42 -0400

    Update Grab_From_Existing_File_ExchangeOnline.ps1

--- Grab_From_Existing_File_ExchangeOnline.ps1
+++ Grab_From_Existing_File_ExchangeOnline.ps1
@@ -1,9 +1,17 @@
+# Author: Alex Cortada
+# Description: A simple Powershell script that pulls the names of users from a file, parses it further by adding addtl info from Exchange Online, then exports to csv.
+# Date: 08.01.2024
+# Version: 2024.08.01
+
+# Script begins here
+
+
 #First line installs the EOM module that allows PoSh to connect to Exchange Online. UserPrinc is gonna be your email as the admin user for MS365 Admin center
 Install-Module ExchangeOnlineManagement
 Import-Module ExchangeOnlineManagment
 Connect-ExchangeOnline -UserPrincipalName alex.cortada@12345.com
-#Add the name of the names of the people you pulled and add it as the Identity below.
 
+#Add the names of the people you pulled and add it as the Identity below.
 Get-DistributionGroup -Identity "A name from the groups text file you created above" | Get-DistributionGroupMember | ft name, primarysmtpaddress >> C:\\members.txt
  
 #Get that CSV export action goodness, baby

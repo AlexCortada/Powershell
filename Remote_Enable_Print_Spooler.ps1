63a8e79a519afedb20683b4d33019abfaedc73ab
Alex Cortada <alexander.cortada@gmail.com>
2024-08-01 19:57:39 -0400

    Update Remote_Enable_Print_Spooler.ps1

--- Remote_Enable_Print_Spooler.ps1
+++ Remote_Enable_Print_Spooler.ps1
@@ -1,3 +1,11 @@
+# Author: Alex Cortada
+# Description: A crazy simple Powershell script that enables the print spooler service on a given machine.
+# Date: 08.01.2024
+# Version: 2024.08.01
+
+# Script begins here
+
+
 $service_name = "Spooler"
 Set-Service -StartupType Automatic $service_name
 Start-Service $service_name
